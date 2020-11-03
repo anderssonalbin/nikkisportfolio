30236e592fcfd54f7dd58cafd19e9508cc2eb276
albin <albin_a@live.se>
2020-11-03 22:01:04 +0100

    deploy

---
+++ src/components/ModalLofberg.vue
@@ -0,0 +1,58 @@
+<template> 
+<div> 
+    <transition name="modal">
+    <div class="modal-mask" @click="close">
+      <div class="modal-wrapper">
+        <div class="modal-container">
+
+          <div class="modal-header">
+            <slot name="header">
+              Lofberg
+            </slot>
+          </div>
+
+          <div class="modal-body">
+            <slot name="body">
+              <div>Text here</div>
+              default bodydefault bodydefault bodydefault bodydefault body
+              default bodydefault bodydefault bodydefault bodydefault bodydefault body
+              default bodydefault bodydefault bodydefault bodydefault bodydefault body
+              default bodydefault bodydefault bodydefault bodydefault bodydefault body
+              default bodydefault bodydefault 
+            </slot>
+          </div>
+
+          <div class="modal-footer">
+          </div>
+        </div>
+      </div>
+    </div>
+  </transition>
+  </div>
+</template>
+
+<script>
+import Vuex from 'vuex'
+import { mapState } from 'vuex'
+export default {
+  name: 'modalL',
+  props: {
+    msg: String
+  },
+  computed: mapState({
+    sml: state => state.showModalL,
+    },
+  ),
+  methods: {
+    close() {
+      this.$store.commit('changeModalL');
+      this.$emit('close');
+    },
+  } 
+}
+</script>
+<style scoped>
+
+</style>
+
+})
---
+++ src/components/ModalWindow.vue
@@ -0,0 +1,60 @@
+<template> 
+<div> 
+    <transition name="modal">
+    <div class="modal-mask" @click="$emit('close')">
+      <div class="modal-wrapper">
+        <div class="modal-container">
+
+          <div class="modal-header">
+            <slot name="header">
+              default header
+            </slot>
+          </div>
+
+          <div class="modal-body">
+            <slot name="body">
+              default bodydefault bodydefault bodydefault bodydefault body
+              default bodydefault bodydefault bodydefault bodydefault bodydefault body
+              default bodydefault bodydefault bodydefault bodydefault bodydefault body
+              default bodydefault bodydefault bodydefault bodydefault bodydefault body
+              default bodydefault bodydefault bodydefault bodydefault bodydefault body
+              default bodydefault bodydefault bodydefault bodydefault bodydefault bodydefault body
+              default bodydefault bodydefault bodydefault bodydefault bodydefault bodydefault body
+              default bodydefault bodydefault bodydefault bodydefault bodydefault bodydefault body
+            </slot>
+          </div>
+
+          <div class="modal-footer">
+            <slot name="footer">
+              default footer
+              <button class="modal-default-button" @click="$emit('close')"> OK </button>
+            </slot>
+          </div>
+        </div>
+      </div>
+    </div>
+  </transition>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'modal',
+  props: {
+    msg: String
+  },
+  data: {
+    showModal: false
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
+
+})
